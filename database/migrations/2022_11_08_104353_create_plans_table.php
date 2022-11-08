94a57a5aae6cc1f4ee9001de9a3183b4fb211a8a
reza <reza@gmail.com>
2022-11-08 14:49:58 +0330

    first commit

---
+++ database/migrations/2022_11_08_104353_create_plans_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('start_city');
+            $table->string('end_city');
+            $table->string('start_terminal');
+            $table->string('end_terminal');
+            $table->integer('move_day');
+            $table->timestamp('move_time');
+            $table->integer('proccess_time');
+            $table->integer('capacity');
+            $table->enum('bus_type', ['vip', 'standard', 'mini']);
+            $table->bigInteger('price');
+            $table->integer('remaining_capacity');
+            $table->json('free_seats');
+            $table->string('search_code')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+};
